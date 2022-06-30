1e8340728105569821775c883ab684701b59c758
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-07-01 00:34:45 +0300

    editting comments

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,38 +1,3 @@
-// import PropTypes from 'prop-types';
-// import styles from '../Form/Form.module.css';
-// import Contact from '../ContactElementList/ContactElementList';
-
-// export const ContactList = ({ contacts, handleRemove }) => {
-//   return (
-//     <div className={styles.wrap}>
-//       {contacts.length
-//         ? contacts.map(({ id, name, number }) => (
-//             <Contact
-//               key={id}
-//               id={id}
-//               name={name}
-//               number={number}
-//               handleRemove={handleRemove}
-//             />
-//           ))
-//         : 'No contacts'}
-//     </div>
-//   );
-// };
-
-// ContactList.propTypes = {
-//   contacts: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       number: PropTypes.string.isRequired,
-//     })
-//   ),
-//   handleRemove: PropTypes.func.isRequired,
-// };
-
-// export default ContactList;
-
 import { useSelector } from 'react-redux';
 import Contact from '../ContactElementList/ContactElementList';
 import styles from '../Form/Form.module.css';
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,20 +1,3 @@
-// import PropTypes from 'prop-types';
-// import styles from '../Form/Form.module.css';
-// export const Filter = ({ value, onChange }) => {
-//   return (
-//     <div className={styles.formLabel}>
-//       <h2>Find contacts by name</h2>
-//       <input className={styles.input} value={value} onChange={onChange} />
-//     </div>
-//   );
-// };
-
-// Filter.propTypes = {
-//   value: PropTypes.string.isRequired,
-//   onChange: PropTypes.func.isRequired,
-// };
-// export default Filter;
-
 import { useSelector, useDispatch } from 'react-redux';
 import { changeFilter } from 'contactStorage/contactSlice';
 
